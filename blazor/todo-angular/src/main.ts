6003ed5461758397299ae80190865ffe5a9272dd
joshivignesh <joshi.vignesh@gmail.com>
2026-03-25 12:30:53 +0000

    add main.ts with zoneless bootstrap

--- blazor/todo-angular/src/main.ts
+++ blazor/todo-angular/src/main.ts
@@ -1,11 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideExperimentalZonelessChangeDetection(),
     provideHttpClient(),
-    provideRouter([])
-  ]
-}).catch(err => console.error(err));
+  ],
+}).catch(console.error);
