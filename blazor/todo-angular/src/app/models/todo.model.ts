2d03e478a88c344a32243daf01cb0f4cd44192fc
joshivignesh <joshi.vignesh@gmail.com>
2026-03-25 12:30:59 +0000

    add todo model interface

--- blazor/todo-angular/src/app/models/todo.model.ts
+++ blazor/todo-angular/src/app/models/todo.model.ts
@@ -2,4 +2,10 @@ export interface Todo {
   id: number;
   title: string;
   isComplete: boolean;
+  createdAt: string;
+}
+
+export interface CreateTodo {
+  title: string;
+  isComplete: boolean;
 }
